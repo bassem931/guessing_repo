51b5efd7f3a1c90addd7201dd2ef727367e65d7d
bassem931 <bti.bassem@gmail.com>
2025-06-08 21:00:27 +0300

    added echo statements to get lines over 20

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 This is a README file.
 ## Guessing Game
-### this was run at Sun Jun  8 20:32:03 EEST 2025
-### Number of lines in this file is: 0
+### this was run at Sun Jun  8 20:59:49 EEST 2025
+### Number of lines in this file is: 21
--- guessing_game.sh
+++ guessing_game.sh
@@ -1,21 +1,21 @@
 function guess {
-while true; do
-    read -p "How many files are in the current directory? " number
-    num_of_files=$(find . -maxdepth 1 -type f | wc -l)
-    if [[ ! $number =~ ^[0-9]+$ ]]; then
-        echo "Please enter a valid number."
-        continue
-    elif [[ $number -lt $num_of_files ]]; then
-        echo "too low"
-    elif [[ $number -gt $num_of_files ]]; then
-        echo "too high"
-    else
-        echo "correct"
-        break
-    fi
-done
+    echo "Welcome to the guessing game!"
+    while true; do
+        echo "How many files are in the current directory? "
+        read number
+        num_of_files=$(find . -maxdepth 1 -type f | wc -l)
+        if [[ ! $number =~ ^[0-9]+$ ]]; then
+            echo "Please enter a valid number."
+            continue
+        elif [[ $number -lt $num_of_files ]]; then
+            echo "too low"
+        elif [[ $number -gt $num_of_files ]]; then
+            echo "too high"
+        else
+            echo "correct number exiting program"
+            break
+        fi
+        echo "wrong guess, try again."
+    done
 }
 guess
-
-
-    
